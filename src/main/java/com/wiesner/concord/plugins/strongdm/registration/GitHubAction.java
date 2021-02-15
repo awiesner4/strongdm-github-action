3056a41f7120bacf055970cff277f30ccc081f1b
Al Wiesner <al.wiesner@starburstdata.com>
2021-02-14 22:13:27 -0500

    v15

--- src/main/java/com/wiesner/concord/plugins/strongdm/registration/GitHubAction.java
+++ src/main/java/com/wiesner/concord/plugins/strongdm/registration/GitHubAction.java
@@ -1,8 +1,6 @@
 package com.wiesner.concord.plugins.strongdm.registration;
 
 import com.walmartlabs.concord.runtime.v2.sdk.MapBackedVariables;
-import com.walmartlabs.concord.runtime.v2.sdk.Task;
-import com.walmartlabs.concord.runtime.v2.sdk.TaskResult;
 import com.walmartlabs.concord.runtime.v2.sdk.Variables;
 
 import java.util.HashMap;
@@ -17,19 +15,18 @@ public class GitHubAction {
         HashMap sdmValuesMap = new HashMap();
 
         for (String envName : env.keySet()) {
-            if (envName.toLowerCase().contains("input_sdm_values_")) {
-                sdmValuesMap.put(envName.replace("input_sdm_values_", ""), env.get(envName));
+            if (envName.contains("sdm_values_")) {
+                sdmValuesMap.put(envName.replace("sdm_values_", ""), env.get(envName));
             }
 
-            if (envName.toLowerCase().contains("input_sdm_base_")) {
-                sdmMap.put(envName.replace("input_sdm_base_", ""), env.get(envName));
+            if (envName.contains("sdm_base_")) {
+                sdmMap.put(envName.replace("sdm_base_", ""), env.get(envName));
             }
         }
 
         sdmMap.put("values", sdmValuesMap);
 
         MapBackedVariables input = new MapBackedVariables(sdmMap);
-
 
 //        TestOutput testOutput = new TestOutput();
 //        testOutput.execute(input);
